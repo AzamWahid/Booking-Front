7fd8552576869e5cac4bb7bb0aa414f2f38e40b1
azam.wahid <azamwahid@outlook.com>
2025-07-13 12:02:33 +0500

    context added 21;13:26

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
+import { SearchContextProvider } from './context/SearchContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <SearchContextProvider>
     <App />
-  </StrictMode>,
+  </SearchContextProvider>,
 )
