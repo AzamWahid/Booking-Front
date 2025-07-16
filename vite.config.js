990584a9401fe92feb82a4a53bfd7a8df7648c90
Muhammad Azam <AzamWahid@outlook.com>
2025-07-16 16:11:21 +0500

    config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8800',
+      '/api': 'https://bookingbackend-production-36e0.up.railway.app',
     },
   },
 })
