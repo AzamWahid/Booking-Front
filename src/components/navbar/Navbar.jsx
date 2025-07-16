0934d986f285f643bbb27ba6ed7cc25b91fdab4c
Muhammad Azam <AzamWahid@outlook.com>
2025-07-16 18:44:26 +0500

    ended

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,22 +1,41 @@
 import React, { useContext } from 'react'
 import './navbar.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../context/AuthContext';
+import axios from 'axios';
 
 const Navbar = () => {
 
+    const navigate = useNavigate();
+    ;
     const { user } = useContext(AuthContext);
 
+    const logoutHandler = async () => {
+
+        try {
+            await axios.post(
+                `${import.meta.env.VITE_API}/auth/logout`,
+                {},
+                { withCredentials: true }
+            );
+            localStorage.removeItem('user');
+            navigate('/login');
+        } catch (err) {
+            console.error("Logout failed", err);
+        }
+    }
     return (
         <div className='navbar'>
             <div className='navContainer'>
                 <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
                     <span className='logo'>AzhiBooking</span>
                 </Link>
-                {user ? user.username :   (<div className="navItems">
-                    <button className="navButton">Register</button>
-                    <button className="navButton">Login</button>
-                </div>)}
+                {user ? (<> <div className="navItems"> Hello {user.username}
+                    <button className="navButton" onClick={logoutHandler}>logout</button></div> </>) :
+                    (<div className="navItems">
+                        <button className="navButton" onClick={() => navigate(`/register`)}>Register</button>
+                        <button className="navButton" onClick={() => navigate(`/login`)}>Login</button>
+                    </div>)}
             </div>
         </div>
     )
